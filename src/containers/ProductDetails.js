1226f9c5fe5743444f320b690572f3ac121a0244
SangeethaShanmugm <sanjuriya22@gmail.com>
2021-12-04 17:08:58 +0530

    first commit

---
+++ src/containers/ProductDetails.js
@@ -0,0 +1,64 @@
+import React, { useEffect } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  selectedProduct,
+  removeSelectedProduct,
+} from "../redux/actions/productsActions";
+const ProductDetails = () => {
+  const { productId } = useParams();
+  let product = useSelector((state) => state.product);
+  const { image, title, price, category, description, rating} = product;
+  const dispatch = useDispatch();
+  const fetchProductDetail = async (id) => {
+    const response = await axios
+      .get(`https://fakestoreapi.com/products/${id}`)
+      .catch((err) => {
+        console.log("Err: ", err);
+      });
+    dispatch(selectedProduct(response.data));
+  };
+
+  useEffect(() => {
+    if (productId && productId !== "") fetchProductDetail(productId);
+    return () => {
+      dispatch(removeSelectedProduct());
+    };
+  }, [productId]);
+  return (
+    <div className="ui grid container">
+      {Object.keys(product).length === 0 ? (
+        <div>...Loading</div>
+      ) : (
+        <div className="ui placeholder segment">
+          <div className="ui two column stackable center aligned grid">
+            
+            <div className="middle aligned row">
+              <div className="column lp">
+                <img className="ui fluid image" src={image} />
+              </div>
+              
+              <div className="column rp">
+               
+                <h1>{title}</h1>
+                <h2>
+                  <a className="label">{price} INR</a>
+                </h2>
+                <h3 className="ui brown  header">Category -{category}</h3>
+                <p>{description}</p>
+                <p>Rating - {rating.rate}</p>
+                <div className="ui vertical animated button" tabIndex="0">                 
+                  <div>Add to Cart</div>
+               
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ProductDetails;
